6868c75c22a81808ad06d0bf3f6aec2132d2b8e7
Meli <meli@Melis-MacBook-Pro.local>
2025-03-27 12:19:06 +0000

    Stored 'all_scores' to avoid multiple calculations

--- code/script.py
+++ code/script.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy.stats import norm
 from Bio import Phylo
 import matplotlib.pyplot as plt 
+from Bio.Phylo.TreeConstruction import DistanceMatrix, DistanceTreeConstructor
 
 #Function reads sequences from a FASTA file
 def read_fasta(file_path):
@@ -18,6 +19,7 @@ def read_fasta(file_path):
 aligner = Align.PairwiseAligner() 
 aligner.match_score, aligner.mismatch_score, aligner.open_gap_score, aligner.extend_gap_score = 1.0, -2.0, -5, -2
 
+
 #Function finds the closest match 
 def find_best_match(dog_breed_sequences, mystery_sequence):
     
@@ -27,10 +29,12 @@ def find_best_match(dog_breed_sequences, mystery_sequence):
     highest_score = -1
     closest_match = ""
     closest_sequence = "" # Stores sequence of the closest match 
+    all_scores = [] 
     
     for record in dog_breed_sequences:
         alignments = aligner.align(mystery_sequence.seq, record.seq)
         score = alignments[0].score 
+        all_scores.append(score)
         
         #
         if score > highest_score:
@@ -38,7 +42,7 @@ def find_best_match(dog_breed_sequences, mystery_sequence):
             closest_match = record.description
             closest_sequence = record.seq
     
-    return closest_match, closest_sequence, highest_score
+    return closest_match, closest_sequence, highest_score, all_scores
     
 #Reads sequences from FASTA files under data folder
 file_path1 = "data/dog_breeds.fa"
@@ -48,11 +52,11 @@ dog_breed_sequences = read_fasta(file_path1)
 mystery_sequence = read_fasta(file_path2)[0]
 
 # Finds the closest match
-closest_match, closest_sequence, similarity_score = find_best_match(dog_breed_sequences, mystery_sequence)
+closest_match, closest_sequence, highest_score, all_scores = find_best_match(dog_breed_sequences, mystery_sequence)
 
 # Prints and saves the output to output.txt file in results folder 
 
-output = f"Closest match: {closest_match}\nSequence: {closest_sequence}\nSimilarity score: {similarity_score}\n"
+output = f"Closest match: {closest_match}\nSequence: {closest_sequence}\nSimilarity score: {highest_score}\n"
 print(output)
 with open("results/Output.txt", "w") as f:
     f.write(output)
@@ -71,10 +75,6 @@ def calculate_p_value(highest_score, all_scores):
     z_score = (highest_score - mean_score) / std_dev
     return 1 - norm.cdf(z_score)
 
-all_scores = [aligner.align(mystery_sequence.seq, record.seq)[0].score for record in dog_breed_sequences]
-
-closest_match = max(all_scores)
-
-p_value = calculate_p_value(closest_match, all_scores)
+p_value = calculate_p_value(highest_score, all_scores)
 print(f"P-value for closest match: {p_value}")
 
